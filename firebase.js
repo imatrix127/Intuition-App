01ad55e17e28d5577c97fe470ce5c4de367fccb9
unknown <disk192@yahoo.com>
2023-04-26 18:58:12 -0400

    Removed firebase config api keys

--- firebase.js
+++ firebase.js
@@ -3,18 +3,12 @@ import { initializeFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmZokJOkKgrZ36UVLWsXCyrty67nWUqjA",
-    authDomain: "gifted-chat-app-ba3ec.firebaseapp.com",
-    projectId: "gifted-chat-app-ba3ec",
-    storageBucket: "gifted-chat-app-ba3ec.appspot.com",
-    messagingSenderId: "701606440702",
-    appId: "1:701606440702:web:39879f0d0a5fa238257faf",
-    measurementId: "G-90PZNSCFN0"
+    // * Put your own firebase config here *
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const db = initializeFirestore(app, {experimentalForceLongPolling: true});
+const db = initializeFirestore(app, { experimentalForceLongPolling: true });
 
 export { db, auth };
